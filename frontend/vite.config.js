351e021937a3b7cf133668d36f14674d35b6fa12
unknown <mannatkathuria23@gmail.com>
2025-12-22 20:06:31 +0530

    Fix GitHub Pages base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-  
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return defineConfig({
-    plugins: [react()],
-    define: {
-      'process.env': env
-    }
-  });
-};
+export default defineConfig({
+  base: '/health-tracker-webapp/',
+  plugins: [react()],
+})
